3fe55bbed3f5a35ec753ac4a6c841b0e085ed15a
Татьяна Тихомирова <tatianatihomirova@MacBook-Air-Tatana.local>
2025-05-10 00:35:29 +0600

    .

--- api/data.js
+++ api/data.js
@@ -1,16 +1,9 @@
+import fs from "fs";
+import path from "path";
+
 export default function handler(req, res) {
-  res.status(200).json([
-    {
-      title: "Product 1",
-      image: "/images/product1.jpg",
-      description: "Description for product 1",
-      price: 19.99,
-    },
-    {
-      title: "Product 2",
-      image: "/images/product2.jpg",
-      description: "Description for product 2",
-      price: 29.99,
-    },
-  ]);
+  const filePath = path.join(__dirname, "db.json");
+  const jsonData = fs.readFileSync(filePath, "utf-8");
+  const data = JSON.parse(jsonData);
+  res.status(200).json(data);
 }
