15a6a1f303c481bf9a83462f76623c26b5051bd2
Татьяна Тихомирова <tatianatihomirova@MacBook-Air-Tatana.local>
2025-05-10 00:41:14 +0600

    .

--- api/data.js
+++ api/data.js
@@ -1,9 +1,8 @@
-import fs from "fs";
-import path from "path";
+import { readFileSync } from "fs";
+import { join } from "path";
 
 export default function handler(req, res) {
-  const filePath = path.join(__dirname, "db.json");
-  const jsonData = fs.readFileSync(filePath, "utf-8");
-  const data = JSON.parse(jsonData);
-  res.status(200).json(data);
+  const filePath = join(process.cwd(), "api", "db.json");
+  const jsonData = JSON.parse(readFileSync(filePath, "utf8"));
+  res.status(200).json(jsonData);
 }
