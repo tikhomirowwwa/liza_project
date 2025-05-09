789d9376c7c46c51277b3fa7e8952cc7ddc10743
Татьяна Тихомирова <tatianatihomirova@MacBook-Air-Tatana.local>
2025-05-10 01:06:34 +0600

    .

--- api/index.js
+++ api/index.js
@@ -1,14 +1,26 @@
-// api/index.js
+// // api/index.js
+// const jsonServer = require("json-server");
+// const path = require("path");
+
+// const server = jsonServer.create();
+// const router = jsonServer.router(path.join(__dirname, "..", "db.json"));
+// const middlewares = jsonServer.defaults();
+
+// server.use(middlewares);
+// server.use(router);
+
+// module.exports = (req, res) => {
+//   server(req, res);
+// };
+
 const jsonServer = require("json-server");
 const path = require("path");
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "..", "db.json"));
+const router = jsonServer.router(path.join(__dirname, "../db.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-module.exports = (req, res) => {
-  server(req, res);
-};
+module.exports = server;
